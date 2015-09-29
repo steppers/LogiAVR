b8cd29d7ff6dde26b9dc1f21141757349fa42435
Steppers <stepz69@gmail.com>
2015-09-29 21:04:37 +0100

    stuff

--- test.asm
+++ test.asm
@@ -1,14 +1,12 @@
 .DEVICE ATmega328P
 
-ldi r16, 17
-ldi r17, 3
-add r16, r17
-ldi r17, 12
-add r16, r17
-bclr 5
-
-ldi r16, 0b10100100
-bst r16, 2
+    ldi r16, 0
+    call inc
 
 hlt:
-rjmp hlt
+    rjmp hlt
+
+inc:
+    ldi r17, 1
+    add r16, r17
+    ret
